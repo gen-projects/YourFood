c6515605eae94fec972030144e4eaaf65d4e0789
USER <blancasuarz@gmail.com>
2024-12-17 14:20:11 -0300

    Model | Produto

---
+++ src/main/java/com/generation/yourfood/model/Produto.java
@@ -0,0 +1,83 @@
+package com.generation.yourfood.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O atributo nome é obrigatório!")
+	private String nome;
+
+	@NotNull(message = "O atributo descrição é obrigatório!")
+	private String descricao;
+
+	@Positive(message = "O preço deve ser um valor positivo!")
+	private Long preco;
+
+	@NotNull(message = "O atributo loja é obrigatório!")
+	private String loja;
+
+	// @ManyToOne(fetch = FetchType.LAZY, mappedBy = "produto", cascade =
+	// CascadeType.REMOVE)
+	// @JsonIgnoreProperties("produto")
+	// private List<Categoria> categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Long preco) {
+		this.preco = preco;
+	}
+
+	public String getLoja() {
+		return loja;
+	}
+
+	public void setLoja(String loja) {
+		this.loja = loja;
+	}
+
+}
